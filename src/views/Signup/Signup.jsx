b25824b1f5b5da698994692a750e8811eaa587d8
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2026-01-14 20:42:23 +0530

    the other remaning input box is added and then the radio btn also added for handling the state the useState hook is used

--- src/views/Signup/Signup.jsx
+++ src/views/Signup/Signup.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import InputBoxs from '../../components/InputBoxs/InputBoxs.jsx'
 
 function Signup() {
+  const [gender, setGender] = useState("");
   return (
     <div>Signup
-      <InputBoxs placeholder="Full Name" type="text" />
+      <InputBoxs type="text" placeholder="Full Name"/>
+      <InputBoxs type="email" placeholder="Enter Email"/>
+      <InputBoxs type="number" placeholder="Enter Contact Number"/>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={gender === "male"}
+            onChange={(e) => setGender(e.target.value)}
+          /> Male
+        </label>
+
+        <label style={{ marginLeft: "10px" }}>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={gender === "female"}
+            onChange={(e) => setGender(e.target.value)}
+          /> Female
+        </label>
+      </div>
+      <InputBoxs type="password" placeholder="Enter password"/>
+      <InputBoxs type="password" placeholder="Confirm Password"/>
+
     </div>
     
   )
