7d263602fa97d2e0c0d38a8113d397c5a7f85356
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2026-01-15 11:10:29 +0530

    validation for email is passed

--- src/views/Signup/Signup.jsx
+++ src/views/Signup/Signup.jsx
@@ -1,71 +1,84 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import InputBoxs from '../../components/InputBoxs/InputBoxs.jsx'
 import Button from "../../components/Button/Button.jsx"
 import { Link } from 'react-router-dom';
 import Login from '../Login/Login.jsx';
+import { use } from 'react';
 
 function Signup() {
   const [name, setName] = useState("");
+  const [email,setEmail] = useState("")
   const [gender, setGender] = useState("");
 
-
   const [nameError, setNameError] = useState("");
-const nameRegex = /^[A-Za-z ]{3,}$/;
-const signupValidation=(e)=>{
+  const [emailError,setEmailError] = useState("")
+
+  const nameRegex = /^[A-Za-z ]{3,}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+  const signupValidation = (e) => {
     e.preventDefault();
 
-      setNameError("");
-       
-       let isValid = true;
+    setNameError("");
+    setEmailError("");
+
+    let isValid = true;
 
     if (!nameRegex.test(name)) {
       setNameError("Enter a valid name (min 3 letters, alphabets only)");
       isValid = false;
     }
-    if (isValid) {
-      alert("Name validation passed");
+    if (!emailRegex.test(email)) {
+      setEmailError("Enter a valid email address");
+      isValid = false;
     }
-}
+
+     if (isValid) {
+      alert("Signup validation passed ✅");
+    }
+    
+  }
 
   return (
     <div>Signup
       <form onSubmit={signupValidation}>
-      <InputBoxs type="text" placeholder="Full Name" value={name} onChange={(e)=>setName(e.target.value)} error={nameError}/>
-      <InputBoxs type="email" placeholder="Enter Email"/>
-      <InputBoxs type="number" placeholder="Enter Contact Number"/>
-      <div>
-        <label>
-          <input
-            type="radio"
-            name="gender"
-            value="male"
-            checked={gender === "male"}
-            onChange={(e) => setGender(e.target.value)}
-          /> Male
-        </label>
+        <InputBoxs type="text" placeholder="Full Name" value={name} onChange={(e) => setName(e.target.value)} error={nameError} />
+        <InputBoxs type="email" placeholder="Enter Email" value={email} onChange={(e)=>setEmail(e.target.value)} error={emailError}/>
+        <InputBoxs type="number" placeholder="Enter Contact Number" />
+        <div>
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={gender === "male"}
+              onChange={(e) => setGender(e.target.value)}
+            /> Male
+          </label>
 
-        <label style={{ marginLeft: "10px" }}>
-          <input
-            type="radio"
-            name="gender"
-            value="female"
-            checked={gender === "female"}
-            onChange={(e) => setGender(e.target.value)}
-          /> Female
-        </label>
-      </div>
-      <InputBoxs type="password" placeholder="Enter password"/>
-      <InputBoxs type="password" placeholder="Confirm Password"/>
-  
-       <Button type="submit" text="Signup"/>
-       </form>
-    <p>
+          <label style={{ marginLeft: "10px" }}>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={gender === "female"}
+              onChange={(e) => setGender(e.target.value)}
+            /> Female
+          </label>
+        </div>
+        <InputBoxs type="password" placeholder="Enter password" />
+        <InputBoxs type="password" placeholder="Confirm Password" />
+
+        <Button type="submit" text="Signup" />
+      </form>
+      <p>
         Already have an account?{" "}
         <Link to="/login">login on existing account</Link>
       </p>
 
     </div>
-    
+
   )
 }
 
