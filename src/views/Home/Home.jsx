4f09185bbde69ff8643570041ee5a232d6e2410b
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2026-01-15 13:44:03 +0530

    feat : the home page logic is implemented where the logined users name and the sihnup and login btn is show

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import "./Home.css";
 
 function Home() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const lastLogin = JSON.parse(localStorage.getItem("lastLoginUser"));
+    if (lastLogin) {
+      setUserName(lastLogin.name);
+    }
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div className="home-container">
+      <div className="home-card">
+        {userName ? (
+          <h2 className="greeting">Hello, {userName}! 👋</h2>
+        ) : (
+          <>
+            <h2 className="home-title">Welcome!</h2>
+            <div className="home-buttons">
+              <Link to="/login">
+                <Button text="Login" />
+              </Link>
+              <Link to="/signup">
+                <Button text="Signup" />
+              </Link>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
