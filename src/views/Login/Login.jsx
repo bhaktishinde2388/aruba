e0fc1a5fedb6bed4ca61cb5481d1edd11fd455be
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2026-01-15 11:58:33 +0530

    feat: the validation is set for the login page

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
+import React,{useState} from 'react'
 import InputBoxs from '../../components/InputBoxs/InputBoxs'
 import Button from '../../components/Button/Button'
 import { Link } from 'react-router-dom'
 
 function Login() {
+   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+   const loginValidation = (e) => {
+    e.preventDefault();
+
+    setEmailError("");
+    setPasswordError("");
+
+    let isValid = true;
+
+       if (!emailRegex.test(email.trim())) {
+      setEmailError("Enter a valid email address");
+      isValid = false;
+    }
+
+    
+    if (password.trim() === "") {
+      setPasswordError("Password is requiredd");
+      isValid = false;
+    }
+
+    if (isValid) {
+      alert("Login validation passed");
+    }
+  }
   return (
      <div className="container">
       <h2>Login</h2>
 
-      <form >
-        <InputBoxs placeholder="Email" type="email"  />
+      <form onSubmit={loginValidation}>
+        <InputBoxs placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}
+          error={emailError} />
         
 
-        <InputBoxs placeholder="Password" type="password"/>
+        <InputBoxs placeholder="Password" type="password" value={password}onChange={(e) => setPassword(e.target.value)} error={passwordError}/>
        
 
-        <Button text="Login" />
+        <Button type="submit"  text="Login" />
       </form>
 
      
